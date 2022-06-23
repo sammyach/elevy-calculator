64981f4e01ab23e8ce798fedca3efe138c95af4e
Sammy <rockarim@hotmail.com>
2022-06-23 19:18:39 +0000

    Auto-generated commit [ci skip]

---
+++ 680.c75a1bc4125aecd8.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkelevy_calculator=self.webpackChunkelevy_calculator||[]).push([[680],{680:(o,n,e)=>{e.r(n),e.d(n,{SplashScreenWeb:()=>l});var s=e(861),u=e(423);class l extends u.Uw{show(_){return(0,s.Z)(function*(){})()}hide(_){return(0,s.Z)(function*(){})()}}}}]);
